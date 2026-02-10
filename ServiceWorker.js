ec8d47b951ba16ea1ebbbba6b063b6ea5d55a3c5
Rayoz Games <rayozgames@gmail.com>
2026-02-10 14:22:41 -0600

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Rayoz Games-Rayoz MiniGames-0.1";
+const contentToCache = [
+    "Build/Web_MiniGames_1.loader.js",
+    "Build/Web_MiniGames_1.framework.js.gz",
+    "Build/Web_MiniGames_1.data.gz",
+    "Build/Web_MiniGames_1.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
